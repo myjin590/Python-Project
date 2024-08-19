a82e15618c06ce51e006c89ad7d4ebc1f4bff2c2
Jinny Yun <85202828+myjin590@users.noreply.github.com>
2024-08-18 20:45:55 -0400

    Update scraper.py
    fixed problems && added getting the user input to search and print it.

--- Python-Project/scraper.py
+++ Python-Project/scraper.py
@@ -8,24 +8,28 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 #open saved list
-df = pd.read_csv("Want to go.csv")
+print("It's running")
+df = pd.read_csv("NYC.csv")
 
 #store url && title into list
 list_link = df["URL"]
 list_title = df["Title"]
 
-
+print(list_link)
 driver = webdriver.Firefox()
+print("opening driver")
 category = ''
 title = ''
 page_num = 1
 categories = []
 titles = []
 res = []
+
 # Navigate to Url
 # Problem to solve : it can't read 'hotel link. why?? -> because it doesn't have classname DkEaL.
 # Problem to solve : store title and category in Pair
 index = 0
+
 for l in list_link:
     try:
            driver.get(l)
@@ -34,15 +38,13 @@ for l in list_link:
                 category = driver.find_element(By.CLASS_NAME, 'DkEaL ')
            else:
                time.sleep(2)
-
     except:
         pass
     
 #problem : it can't retreive category.text --> error ---> solved
-#problem : get NAME of the place and store it into list
+#problem : get NAME of the place and store it into list --> solved
     if category:
         try:
-            print(index)
             categories += [category.text]
             res.append((category.text, list_title[index]))
             
@@ -53,14 +55,17 @@ for l in list_link:
         pass
     
     index += 1
-    
     page_num =+ 1
-#..
-#get needed result
 
 print(res)
 
-# result = []
-# for i in categories:
-#     if i == "Coffee shop" or i == "Cafe":
-#         print(i)
+userInput = input("Which category you want to see?")
+
+result = []
+for i in res:
+    #i[0] == category
+    #i[1] == name of the place
+    if lower(i[0]) == lower(userInput):
+        result.append(i[1])
+
+print(result)
